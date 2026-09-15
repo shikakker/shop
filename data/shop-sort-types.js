ffcaa9ea1ea39c5805c5a88a33b4c835ba1245ea
Egor Denisov <designer.egor@ya.ru>
2026-09-15 07:45:38 -0400

    refactor: share shop sort options outside Studio

---
+++ data/shop-sort-types.js
@@ -0,0 +1,9 @@
+export const sortTypes = [
+  { title: 'Featured', value: 'featured' },
+  { title: 'Newest', value: 'dateDesc' },
+  { title: 'Oldest', value: 'dateAsc' },
+  { title: 'Price: High to Low', value: 'priceDesc' },
+  { title: 'Price: Low to High', value: 'priceAsc' },
+  { title: 'Alpha: A – Z', value: 'alphaAsc' },
+  { title: 'Alpha: Z – A', value: 'alphaDesc' },
+]
